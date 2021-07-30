4c4be76e17152124b840d98df6d5a8357e370e4e
Tomonori Ikeyama <ikeyama.tomonori@gmail.com>
2021-07-30 22:38:14 +0900

    Add swagger generator

---
+++ test/EfRest.Swagger.Test/BookDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using EfRest.Example.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EfRest.Swagger.Test
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; } = null!;
+
+        public DbSet<Book> Books { get; set; } = null!;
+
+        [JsonPropertyName("books/details")]
+        public DbSet<BookDetail> BookDetails { get; set; } = null!;
+
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+
+        public string DbName { get; }
+
+        public BookDbContext()
+        {
+            DbName = Guid.NewGuid().ToString();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseInMemoryDatabase(DbName)
+                .ConfigureWarnings(builder =>
+                {
+                    builder.Log(InMemoryEventId.TransactionIgnoredWarning);
+                })
+                .LogTo(Console.WriteLine)
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ test/EfRest.Swagger.Test/SwaggerGenTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Microsoft.OpenApi.Writers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EfRest.Swagger.Test
+{
+    [TestClass]
+    public class SwaggerGenTest
+    {
+        [TestMethod]
+        public void GenerateSwagger()
+        {
+            using var db = new BookDbContext();
+            var jsonSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            var swaggerGen = new EfRestSwagger(db, jsonSerializerOptions);
+            var swagger = swaggerGen.GetSwagger();
+
+            Assert.IsNotNull(swagger.Paths.FirstOrDefault(p => p.Key == "/publishers"));
+        }
+    }
+}
